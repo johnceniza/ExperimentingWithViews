dc69f4f774706c8ba43491881801c7f49f755a7a
John Ceniza <jc@johnceniza.com>
2015-06-26 01:53:03 -0400

    initial commit. showing view modally

--- ExperimentingWithViews/ViewController.swift
+++ ExperimentingWithViews/ViewController.swift
@@ -14,12 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func experiment(sender: UIButton) {
+        let nextController = UIImagePickerController()
+        self.presentViewController(nextController, animated: true, completion: nil)
     }
-
-
 }
 
